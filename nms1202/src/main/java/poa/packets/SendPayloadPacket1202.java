810fa9f64cc09e7910c0c528f76693af6ae6f37d
tsukieru904 <zhouyuyuou@gmail.com>
2026-08-12 14:53:42 +0800

    Fix: Add Plugin parameter to sendPacket() calls

--- nms1202/src/main/java/poa/packets/SendPayloadPacket1202.java
+++ nms1202/src/main/java/poa/packets/SendPayloadPacket1202.java
@@ -8,20 +8,21 @@ import org.bukkit.Color;
 import org.bukkit.Location;
 import org.bukkit.craftbukkit.v1_20_R2.block.CraftBlock;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 
 public class SendPayloadPacket1202 {
 
-    public static void sendGameTestMarker(Player player, Location location, String text, Color color, int durationMs) {
+    public static void sendGameTestMarker(Plugin plugin, Player player, Location location, String text, Color color, int durationMs) {
         BlockPos pos = ((CraftBlock) location.getBlock()).getPosition();
 
         GameTestAddMarkerDebugPayload marker = new GameTestAddMarkerDebugPayload(pos, color.asARGB(), text, durationMs);
 
-        SendPacket1202.sendPacket(player, new ClientboundCustomPayloadPacket(marker));
+        SendPacket1202.sendPacket(plugin, player, new ClientboundCustomPayloadPacket(marker));
 
     }
 
-    public static void sendBrandPayload(Player player, String brand){
-        SendPacket1202.sendPacket(player, new ClientboundCustomPayloadPacket(new BrandPayload(brand)));
+    public static void sendBrandPayload(Plugin plugin, Player player, String brand){
+        SendPacket1202.sendPacket(plugin, player, new ClientboundCustomPayloadPacket(new BrandPayload(brand)));
     }
 
 }
